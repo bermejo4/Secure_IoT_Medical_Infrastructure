dec6aac2794b64d0aeec4149ee449f566fae58ea
bermejo4 <minijai72@gmail.com>
2022-07-23 02:21:49 +0200

    Update prueba_server_Aes_desencrypter.py

--- prueba_server_Aes_desencrypter.py
+++ prueba_server_Aes_desencrypter.py
@@ -7,7 +7,7 @@ print(binascii.b2a_base64(ciphertext))
 print(binascii.hexlify(ciphertext))
 print(ciphertext)
 
-cifrado="86858d74ed5eaff88c1428b89afca624".encode("utf8")
+cifrado="0f1d1d12a6fc85ae06ad25c817d25595".encode("utf8")
 cifra=binascii.unhexlify(cifrado)
 obj2 = AES.new('AAAAAAAAAAAAAAAA'.encode("utf8"), AES.MODE_CBC, 'BBBBBBBBBBBBBBBB'.encode("utf8"))
 print(obj2.decrypt(cifra))
