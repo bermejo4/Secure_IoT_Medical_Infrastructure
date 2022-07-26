c5392499ed315a21c08639f3e528335d3c74416f
bermejo4 <minijai72@gmail.com>
2022-07-26 23:51:14 +0200

    changes

--- streamlit_app/main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-header = st.container()
-dataset = st.container()
-features = st.container()
-modelTraining = st.container()
-
-with header:
-    st.title("IoT application")
