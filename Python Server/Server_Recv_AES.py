d9568f6f3bba4ce6b9e49317638d487db1072dc0
bermejo4 <minijai72@gmail.com>
2022-07-27 14:19:45 +0200

    PythonServer conecta con EMQX y crea topics

--- Python Server/Server_Recv_AES.py
+++ Python Server/Server_Recv_AES.py
@@ -4,6 +4,10 @@ import json
 from Crypto.Cipher import AES
 import binascii
 import sys
+import random
+import time
+from paho.mqtt import client as mqtt_client
+
 
 def segmentate_in_ciphered_strings(cadena):
     first=''
@@ -36,6 +40,19 @@ def data_from_pico_desencrypter(text, key, iv):
     data_decrypted=data_decrypted.replace('&','')
     return data_decrypted
 
+
+def check_words_in_decrypted_string(text):
+    if "\"Temp\"" in text:
+        if "\"TempMcu\"" in text:
+            if "\"PulseSig\"" in text:
+                if "\"Acel_x\"" in text:
+                    if "\"Acel_y\"" in text:
+                        if "\"Acel_z\"" in text:
+                            if "\"TempMpu\"" in text:
+                                return True
+    return False
+
+
 def autentication_and_ID_assignation(ciphertext):
     #this function read DEVICES_KEYS.txt and check the ciphertext with each key in the file to identify devices.
     json_dict={}
@@ -53,7 +70,7 @@ def autentication_and_ID_assignation(ciphertext):
         key=json_dict[a][0]
         iv=json_dict[a][1]
         print(data_from_pico_desencrypter(ciphertext, key, iv))
-        if "Temp" in data_from_pico_desencrypter(ciphertext, key, iv):
+        if check_words_in_decrypted_string(data_from_pico_desencrypter(ciphertext, key, iv)):
             print("Founded: "+ a)
             device_searched=a
             break
@@ -62,47 +79,75 @@ def autentication_and_ID_assignation(ciphertext):
     return device_searched, json_dict[device_searched][0], json_dict[device_searched][1]
 
 
+def connect_mqtt(client_id, broker, port):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    #client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client, topic, msg):
+    #msg = f"messages: {msg_count}"
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+
 class Servidor:
     def __init__(self):
         self.IP_ADDRESS='0.0.0.0'
-        self.PORT_ADDRESS=9999
+        self.PORT_ADDRESS=9998
         self.SERVER_ADDRESS=(self.IP_ADDRESS, self.PORT_ADDRESS)
         #self.fecha=str(date.today().day)+'/'+str(date.today().month)+'/'+str(date.today().year)
         #self.hora=str(datetime.now().hour)+':'+str(datetime.now().minute)
 
 class DataPico:
     def __init__(self):
-        self.temp_array_y = []
-        self.temp_array_x = []
-        self.temp_mcu_array_y = []
-        self.temp_mcu_array_x = []
-        self.pulse_array_y=[]
-        self.pulse_array_x = []
-        self.x_acel_array_y=[]
-        self.x_acel_array_x = []
-        self.y_acel_array_y=[]
-        self.y_acel_array_x=[]
-        self.z_acel_array_y=[]
-        self.z_acel_array_x = []
+        self.temp_array = []
+        self.temp_mcu_array = []
+        self.pulse_array=[]
+        self.x_acel_array=[]
+        self.y_acel_array=[]
+        self.z_acel_array = []
+        self.temp_mpu_array = []
 
-
-ciphertext="b102ea0ad185e800792364c204a18248+eddde8a2d9f314ba1181677d75192b83+f1b05010657ce23b94d935b362ba3910+6b9c88c72dfa3a247d02c24e8ffdaf4c+6968a09c1fb9d64c364d76be162a0b14+49f657df4bcf47479cab8d034a0c98cc+cafa765f1eaa23932dd5c93035eddb71+c8161a67c0467a922d64c0a1869c2ec5+c42e0f042a319174ce185ee19fa73445+7afa9091bc6336d5973a45d9895f4b67"
-device_id, key, iv = autentication_and_ID_assignation(ciphertext)
-if device_id=="Not_found_dev":
-    print("POSSIBLE HACKING ATTACK DETECTED!!! SERVER DOWN")
-    sys.exit()
-print(device_id, key, iv )
-
+class Mqtt_publisher:
+    def __init__(self, device_id):
+        self.broker = 'localhost'
+        self.port = 1883
+        self.topic = [
+            "Pico/"+device_id+"/Physiological_Data/Temperature", #0
+            "Pico/"+device_id+"/Physiological_Data/Pulse_Signal", #1
+            "Pico/"+device_id+"/Physiological_Data/Accelerometer/x", #2
+            "Pico/"+device_id+"/Physiological_Data/Accelerometer/y", #3
+            "Pico/"+device_id+"/Physiological_Data/Accelerometer/z", #4
+            "Pico/"+device_id+"/Internal_Device_Data/Temperature/MCU", #5
+            "Pico/"+device_id+"/Internal_Device_Data/Temperature/MPU" #6
+        ]
+        self.client_id = device_id
+        # username = 'emqx'
+        # password = 'public'
 
 server=Servidor()
 socketTCP=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socketTCP.bind(server.SERVER_ADDRESS)
 socketTCP.listen(2)
 solicitud = ''
-print('Servidor escuchando en el puerto: ' + str(server.PORT_ADDRESS))
+print('Server listening in the port: ' + str(server.PORT_ADDRESS))
 conexion, CLIENT_ADDRESS = socketTCP.accept()
 
 atentication_check=False
+
 
 while True:
         solicitud = ''
@@ -112,29 +157,25 @@ while True:
         data_from_pico = solicitud.decode()
         print(data_from_pico)
         # data_json=json.loads(data_from_pico)
-        print('RECIBOO:'+str(data_from_pico))
+        print('RECIBO:'+str(data_from_pico))
         ###----------------checkear si esto funciona:
         if not atentication_check:
-            device_id, key, iv = autentication_and_ID_assignation(str(data_from_pico))
+            device_id, KEY, IV = autentication_and_ID_assignation(str(data_from_pico))
             if device_id=="Not_found_dev":
                 print("POSSIBLE HACKING ATTACK DETECTED!!! SERVER DOWN")
                 sys.exit()
-            print(device_id, key, iv )
+            mqtt_publisher=Mqtt_publisher(device_id)
+            client_mqtt_publisher = connect_mqtt(mqtt_publisher.client_id , mqtt_publisher.broker, mqtt_publisher.port)
+            print(device_id, KEY, IV)
             atentication_check=True
 
-        # data_pico_saved.temp_array[counter]=data_json["Temp"]
-        # print(data_pico_saved)
-        # print('Temperature: '+data_json["Temp"]+"ºC")
-        # print('Temperature from mcu: '+data_json["TempMcu"]+"ºC")
-        # print('Pulse signal: ' + data_json["PulseSig"] + " Volts")
-        # print('Acelerometer: (' + data_json["Acel_x"] + ","+data_json["Acel_y"]+","+data_json["Acel_z"]+")")
-        if "+" in data_from_pico:
-            print(data_from_pico_desencrypter(data_from_pico, KEY, IV))
-            #data_json = json.loads(data_from_pico_desencrypter(data_from_pico))
-            print('RECIBOO:' + str(data_from_pico))
-            #data_pico_saved.temp_array_y.append(float(data_json["Temp"]))
-            #data_pico_saved.temp_mcu_array_y.append(float(data_json["TempMcu"]))
-            # data_pico_saved.pulse_array_y.append(float(data_json["PulseSig"]))
-            # data_pico_saved.x_acel_array_y.append(float(data_json["Acel_x"]))
-            # data_pico_saved.y_acel_array_y.append(float(data_json["Acel_y"]))
-            # data_pico_saved.z_acel_array_y.append(float(data_json["Acel_z"]))
+        print('RECIBOO:' + str(data_from_pico))
+        data_string=data_from_pico_desencrypter(data_from_pico, KEY, IV)
+        pico=json.loads(data_string)
+        publish(client_mqtt_publisher,mqtt_publisher.topic[0] , str(pico["Temp"]))
+        publish(client_mqtt_publisher,mqtt_publisher.topic[1] , str(pico["PulseSig"]))
+        publish(client_mqtt_publisher,mqtt_publisher.topic[2] , str(pico["Acel_x"]))
+        publish(client_mqtt_publisher,mqtt_publisher.topic[3] , str(pico["Acel_y"]))
+        publish(client_mqtt_publisher,mqtt_publisher.topic[4] , str(pico["Acel_z"]))
+        publish(client_mqtt_publisher,mqtt_publisher.topic[5] , str(pico["TempMcu"]))
+        publish(client_mqtt_publisher,mqtt_publisher.topic[5] , str(pico["TempMpu"]))
