c5c080523d9575a1d2c82b92e56dff905085a3e1
bermejo4 <minijai72@gmail.com>
2022-08-17 12:51:24 +0200

    Update DataExterCollector.py

--- Raspberry Pi Pico/DataExterCollector.py
+++ Raspberry Pi Pico/DataExterCollector.py
@@ -2,8 +2,9 @@ from mpu6050 import MPU6050
 from machine import Pin, ADC, Signal
 import utime
 import machine
-
-
+import onewire
+from ds18x20 import DS18X20
+import binascii
 
 class pico_data:
     def __init__(self, temp=None, temp_mcu=None, pulse_sig=None, cord_x=None, cord_y=None, cord_z=None, temp_mpu=None):
@@ -69,6 +70,22 @@ def data_collector():
     rawValue = sensortemp.read_u16() * factorconversion
     temperatura_mcu = 27 - (rawValue - 0.706) / 0.001721
     pico.temp_mcu=temperatura_mcu
+    #--------------------------------------------------
+    #SENSOR TEMPERATURE
+    ow = onewire.OneWire(Pin(16)) #Prepara GPIO4 para usar con OneWire
+    sensor = DS18X20(ow) #define un sensor en ese pin
+    direcciones = sensor.scan()  #Lee el ID del sensor conectado
+    id=direcciones[0]
+    #Pasa el ID a formato de texto e imprime
+    idHex = binascii.hexlify(bytearray(id))
+    print ("ID=",idHex)
+    #Lee e imprime la temperatura cada 1 segundo
+    #while (True):
+    sensor.convert_temp ()
+        #sleep (1)
+    temperatura = sensor.read_temp (id)
+        #print (temperatura)
+    pico.temp=temperatura
     #--------------------------------------------------
     #Returnind data in JSON Format:
     return pico.JSON_transform()
